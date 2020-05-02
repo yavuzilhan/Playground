bef66a05ca1f98bcae2eee7058da21ed523317a4
Yavuz İlhan <yavuzilhan@yahoo.com>
2020-05-02 20:42:56 +0300

    02.05.2020

---
+++ js/message.js
@@ -0,0 +1,13 @@
+alert("Javascript yüklendi!")
+
+function Call() {
+    alert("Beni hangi fonksiyon çağırdı?");
+}
+
+function Goodbye() {
+    alert("Güle güle");
+}
+
+function Welcome() {
+    alert("Hoşgeldiniz");
+}
