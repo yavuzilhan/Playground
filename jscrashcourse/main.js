ff674a8d65d3381938d65a0e6861e9aae7de9f16
Yavuz İlhan <yavuzilhan@yahoo.com>
2020-09-05 22:17:41 +0300

    Karma

---
+++ jscrashcourse/main.js
@@ -0,0 +1,10 @@
+const x = 1;
+const y = 10;
+
+if (x === 10) {
+  console.log("x is 10");
+} else if (x > 10) {
+  console.log("x is greater than 10");
+} else {
+  console.log("x is less than 10");
+}
