f2ffd799810bf3f6ae98be3270152b1ddf9e02e2
Yavuz İlhan <yavuzilhan@yahoo.com>
2020-09-09 13:28:44 +0300

    sass crash course

--- jscrashcourse/main.js
+++ jscrashcourse/main.js
@@ -1,10 +1,27 @@
-const x = 1;
-const y = 10;
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const userList = document.querySelector("#users");
 
-if (x === 10) {
-  console.log("x is 10");
-} else if (x > 10) {
-  console.log("x is greater than 10");
-} else {
-  console.log("x is less than 10");
+myForm.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  if (nameInput.value === "" || emailInput.value === "") {
+    msg.style.color = "red";
+    msg.innerHTML = "Please enter all fields";
+
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(nameInput.value, emailInput.value));
+
+    userList.appendChild(li);
+
+    // Clear fields
+    nameInput.value = "";
+    emailInput.value = "";
+  }
 }
