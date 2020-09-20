ec923ef2ab99d5acddaccbadac8918297d83fff1
Yavuz İlhan <yavuzilhan@yahoo.com>
2020-09-20 21:25:23 +0300

    reactcrasshcourse

---
+++ reactcrashcourse/src/components/TodoItem.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export class TodoItem extends Component {
+  getStyle = () => {
+    return {
+      background: "#f4f4f4",
+      padding: "10px",
+      borderBottom: "1px dotted #ccc",
+      textDecoration: this.props.todo.completed ? "line-through" : "none",
+    };
+    // if (this.props.todo.completed) {
+    //   return {
+    //     textDecoration: "line-through",
+    //   };
+    // } else {
+    //   return {
+    //     textDecoration: "none",
+    //   };
+    // }
+  };
+
+  render() {
+    const { id, title } = this.props.todo;
+    return (
+      <div style={this.getStyle()}>
+        <p>
+          <input
+            type="checkbox"
+            onChange={this.props.markComplete.bind(this, id)}
+          />{" "}
+          {title}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>
+            x
+          </button>
+        </p>
+      </div>
+    );
+  }
+}
+
+// TodoItem.PropTypes = {
+//   todos: PropTypes.object.isRequired,
+// };
+
+const itemStyle = {
+  backgroundColor: "#f4f4f4",
+};
+
+const btnStyle = {
+  background: "#ff0000",
+  color: "#fff",
+  border: "none",
+  padding: "5px 9px",
+  borderRadius: "50%",
+  cursor: "pointer",
+  float: "right",
+};
+
+export default TodoItem;
